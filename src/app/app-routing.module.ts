5f4ff9fdb1e94ad36e28f9813797f99ee434558d
Denise Anjos <deniseb.anjos@outlook.com>
2021-04-08 10:20:47 -0300

    fix(rota/login): removido conflitos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdmComponent } from './pages/adm/adm.component';
-=======
+
 import { LoginComponent } from './components/login/login.component';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
 import { ProdutosComponent } from './pages/produtos/produtos.component';
@@ -16,7 +16,7 @@ const routes: Routes = [
   {path: 'produtos', component: ProdutosComponent},
   {path: 'sobre-projeto', component: SobreProjetoComponent},
 
-  {path: 'adm', component: AdmComponent}
+  {path: 'adm', component: AdmComponent},
 
   {path: 'login', component: LoginComponent}
   
